b77561abaa3172383fff85056c55fd51bdc255a7
mrfunbarn <bmbellat@gmail.com>
2015-09-22 15:42:48 -0600

    Changed github url in setup.py to my current new url.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(name='chiron-spec-tools',
       description='Some tools to view and normalize SMARTS 1.5m CHIRON data',
       author='Brandon Bell',
       author_email='bmbellat@gmail.com',
-      url='git://github.com/MrFunPy/chiron-spec-tools.git',
+      url='git://github.com/MrFunBarn/chiron-spec-tools.git',
       py_modules=['interactivenorm'],
       scripts=['median-chiron','inspect-chiron','normalize-chiron']
       )
